0b57daa51da17365f19347810e8140d405b8d055
Oleg <serv_all@mail.ru>
2022-11-12 15:51:57 +0300

    Простые классы для вычисления площадей круга и треугольника выполнены. Всё будет в ветке master

---
+++ ComputeArea/Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputeArea
+{
+    public class Circle : AbstractFigure
+    {
+        public double radius { get; private set; }
+
+        public Circle() { }
+
+        public Circle(double Radius)
+        {
+            if (Radius < 0) // проверка на корректность ввода радиуса
+            {
+                throw new ArgumentException("Check input arguments");
+            }
+            radius = Radius;
+        }
+
+        public override double Area()
+        {
+            return radius * radius * 3.14;
+        }
+    }
+}
---
+++ ComputeArea/Triangle.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputeArea
+{
+    public class Triangle : AbstractFigure
+    {
+        public double a { get; private set; }
+        public double b { get; private set; }
+        public double c { get; private set; }
+
+        private double area = -1; //кэшируем значение чтоб не вычислять каждый раз
+
+        private bool? isSquare = null;
+
+        public Triangle() { }
+
+        public Triangle(double A, double B, double C)
+        {
+            if (A < 0 || B < 0 || C < 0)
+            {
+                throw new ArgumentException("Check input arguments");
+            }
+            a = A;
+            b = B;
+            c = C;
+        }
+
+        public override double Area()
+        {
+            if (area == -1)
+            {
+                double halfPerimeter = (a + b + c) / 2;
+                area = Math.Sqrt(halfPerimeter *
+                                (halfPerimeter - a) *
+                                (halfPerimeter - b) *
+                                (halfPerimeter - c));
+            }
+            
+            return area;
+        }
+
+        public bool isTriangleSquare()
+        {
+            if (!isSquare.HasValue)
+            {
+
+
+
+
+
+            }
+
+            return isSquare.Value;
+        }
+
+    }
+}
